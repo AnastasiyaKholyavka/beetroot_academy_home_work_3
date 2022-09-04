29d032fbbb8618b893c5a71440ae1cd85bfc61ce
AnastasiyaKholyavka <108723400+AnastasiyaKholyavka@users.noreply.github.com>
2022-09-04 14:48:48 +0300

    Add files via upload

---
+++ anastasiya_kholyavka_hw_3_task_2.py
@@ -0,0 +1,13 @@
+"""
+anastasiya_kholyavka
+HW_3_task_2
+
+Save your first and last name as separate variables, 
+then use string concatenation to add them together with a white space 
+in between and print a greeting.
+
+"""
+first_name = "Anastasiya"
+last_name = "Kholyavka"
+
+print("Hello," + " " + first_name + " " + last_name + "!")
