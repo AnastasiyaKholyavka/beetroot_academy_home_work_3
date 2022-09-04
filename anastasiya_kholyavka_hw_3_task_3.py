1abafcc1209a47801b0d21f25851c2b870477301
AnastasiyaKholyavka <108723400+AnastasiyaKholyavka@users.noreply.github.com>
2022-09-04 15:02:05 +0300

    Add files via upload

---
+++ anastasiya_kholyavka_hw_3_task_3.py
@@ -0,0 +1,30 @@
+"""
+anastasiya_kholyavka
+HW_3_task_3
+
+Make a program with 2 numbers saved in separate variables a and b, 
+then print the result for each of the following: 
+
+Addition
+Subtraction
+Division
+Multiplication
+Exponent (Power)
+Modulus
+Floor division
+
+"""
+
+#Чи можна зробити всі ці дії через одне print? 
+#Розділити у окремі рядки з допомогою \n не вийшло.
+
+a = 50
+b = 5
+
+print(a+b)
+print(a-b)
+print(a//b)
+print(a*b)
+print(a**b)
+print(abs(a-b))
+print(a%b)
